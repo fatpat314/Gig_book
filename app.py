f1d4320b55233666da34af339b599c9f5419f502
Patrick <w.patrick.kelly@gmail.com>
2020-01-06 16:35:50 -0800

    Navigation is working for swing.

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask (__name__)
 
 @app.route('/')
 def index():
     """Return homepage."""
-    return render_template('home.html', msg='Flask in Cool!!!')
+    return render_template('home.html', msg='Welcome to Gig_book')
+
+@app.route('/swing')
+def swing():
+    """Show Swing songs"""
+    return render_template('swing.html')
+
+@app.route('/Benny_Goodman')
+def Benny_Goodman():
+    return render_template('Benny_Goodman.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)

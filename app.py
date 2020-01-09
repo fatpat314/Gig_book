508eae05b799d258091f4756a1bc265ebcd7ba95
Patrick <w.patrick.kelly@gmail.com>
2020-01-09 13:47:59 -0800

    heroku

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from bson.objectid import ObjectId
 import os
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Gig_book')
-client = MongoClient() #MongoClient(host=f'{host}?retryWrites=false')
-db = client.Gig_book #client.get_default_database()
+client = MongoClient(host=f'{host}?retryWrites=false') #MongoClient()
+db = client.get_default_database() #client.Gig_book
 songs_collection = db.songs
 comments = db.comments
 
